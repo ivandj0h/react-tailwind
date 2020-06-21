2af30de1a90420af170276c7ac05aaf859c4ea99
ivandi1980 <ivandi.djoh@gmail.com>
2020-06-22 03:18:22 +0700

    components update

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,39 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ImageCard = ({ image }) => {
-  const tags = image.tags.split(',');
+const ImageSearch = ({ searchText }) => {
+  const [text, setText] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    searchText(text);
+  }
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={image.webformatURL} alt="" className="w-full"/>
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by {image.user}
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            {image.views}
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            {image.downloads}
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            {image.likes}
-          </li>
-        </ul>
+    <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+      <form onSubmit={onSubmit} className="w-full max-w-sm">
+        <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+        <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image Term..." />
+        <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+      Search
+      </button>
       </div>
-      <div className="px-6 py-4">
-        {tags.map((tag, index) => (
-          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #{tag}
-        </span>
-        ))}
-      </div>
-    </div>
+      </form>
+		</div>
   )
 }
 
-export default ImageCard;
+export default ImageSearch;
